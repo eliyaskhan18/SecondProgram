8c36127f760a9ddcd36040881f52f7bdda1ba83e
eliyaskhan18 <mohd.eliyas.sid@gmail.com>
2019-01-10 14:36:36 +0530

    First program executing

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,9 @@
 public class helloworld{
 	public static void main(String args[]){
-		System.out.println("Hello world");
+		System.out.println("multiplication---------------");
+		int num = 5;
+		for (int i =1; i <=10; i++){
+			System.out.println(num+" * "+i+ " =="+(num*i));
+		}
 	}
 }
